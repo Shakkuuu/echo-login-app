7d50bf9e0357e2bd00c1c4edb72d6587a4e70780
Shakkuuu <shakku0521@gmail.com>
2023-06-26 14:19:16 +0900

    echoでhtmlを返してみた。

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+)
+
+type TemplateRender struct {
+	templates *template.Template
+}
+
+func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+
+	renderer := &TemplateRender{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	e.Renderer = renderer
+
+	e.GET("/", Hello)
+	e.GET("/map", Mappp)
+	e.GET("struct", Structtt)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func Mappp(c echo.Context) error {
+	m := map[string]interface{}{
+		"name": "Yamada",
+		"age":  "31",
+	}
+	return c.Render(http.StatusOK, "yamada", m)
+}
+
+func Structtt(c echo.Context) error {
+	v := struct {
+		Name string
+		Age  int
+	}{
+		Name: "Kojima",
+		Age:  96,
+	}
+	return c.Render(http.StatusOK, "kojima", v)
+}
+
+func Hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello")
+}
